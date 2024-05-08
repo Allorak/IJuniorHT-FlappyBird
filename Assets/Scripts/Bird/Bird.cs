c9f298c7054f8f68b217f44d14bf6f590efa156d
Моисеев Михаил <allorakspam@gmail.com>
2024-05-08 22:08:33 +0300

    Added  ground

--- Assets/Scripts/Bird/Bird.cs
+++ Assets/Scripts/Bird/Bird.cs
@@ -1,5 +1,23 @@
+using System;
 using UnityEngine;
 
+[RequireComponent(typeof(BirdMover))]
+[RequireComponent(typeof(BirdCollisionHandler))]
 public class Bird : MonoBehaviour
 {
+    private BirdMover _mover;
+    private BirdCollisionHandler _collisionHandler;
+
+    public event Action GameOver;
+
+    private void Awake()
+    {
+        _mover = GetComponent<BirdMover>();
+        _collisionHandler = GetComponent<BirdCollisionHandler>();
+    }
+
+    public void Reset()
+    {
+        
+    }
 }
